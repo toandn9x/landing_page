d5af1d09c0bea54bad81ea4a2cb1343c35251060
toandn <toandn@softdreams.vn>
2022-04-14 22:46:01 +0700

    fix api

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -21,7 +21,7 @@ class apiController extends Controller
             ->get()->toArray();
         $contents = DB::table('contents')
             ->join('menus', 'menus.id', '=','contents.id_menu')
-            ->select('contents.id', 'contents.description', 'contents.content', 'contents.img', 'menus.name as m_name', 'menus.id as m_id', 'menus.description as m_description')
+            ->select('menus.name as m_name', 'menus.description as m_description', 'contents.description', 'contents.content', 'contents.img')
             ->where('contents.status', 1)
             ->orderBy('menus.m_order', 'asc')
             ->get();
@@ -32,12 +32,16 @@ class apiController extends Controller
         foreach ($contents as $key => $item) {
             $arr_content[$item['m_name']][$key] = $item;
         }
+        foreach ($arr_content as $key => $value) {
+            $arr_content2[] = array_values($value);
+
+        }
         $page2 = DB::table('news')->select('id','description','content','img')
             ->where('status', 1)
             ->orderBy('id', 'desc')
             ->get()->toArray();
         $settings = DB::table('settings')->first();
-        return response()->json(['menus' => $menus, 'contents' => $arr_content, 'page2' => $page2,'settings' => $settings]);
+        return response()->json(['menus' => $menus, 'contents' => $arr_content2, 'page2' => $page2,'settings' => $settings]);
 
     }
 }
